607e654be4d74e4ebe96391f2b76d8e587191cbf
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2026-01-30 16:33:31 +0300

    Updating packages

--- app/src/services/supabase.ts
+++ app/src/services/supabase.ts
@@ -1,7 +1,17 @@
-// src/services/supabase.ts
-import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient, processLock } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';  // from Supabase dashboard
-const supabaseAnonKey = 'YOUR_ANON_KEY';  // from Supabase dashboard
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+    process.env.EXPO_PUBLIC_SUPABASE_URL!,
+    process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+    {
+        auth: {
+            storage: AsyncStorage,
+            autoRefreshToken: true,
+            persistSession: true,
+            detectSessionInUrl: false,
+            lock: processLock,
+        },
+    })
+        
